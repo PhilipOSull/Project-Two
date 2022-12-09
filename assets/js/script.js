acee453948cac03bef0bbb503c294f52544777fe
PhilipOSull <philiposullivan11@gmail.com>
2022-12-09 20:23:43 +0000

    Make the start of the game, user can guess and it records the guesses along with how many guesses they had. Add alert if guess is lower than the correct answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,39 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     let btn = document.getElementById("btn--Guess");
 
     btn.addEventListener("click", runGame);
 })
 
-let computerAnswer = Math.floor(Math.random() * 100) +1;
 let numGuesses = document.getElementById("numGuesses");
 let numGuessed = document.getElementById("numGuessed");
+let computerAnswer = Math.floor(Math.random()*100) + 1;
+let highLowMsg = document.getElementById("highLow");
 
-let 
+
 
 function runGame(event) {
+
+    // event.preventDefault();
+
     let userGuess = document.getElementById("guessArea").value;
     if(userGuess < 1 || userGuess > 100){
         alert("Please enter a number between 1 and 100.");
-    } else {
-        numGuessed.push(userGuess);
-        numGuesses+= 1;
+    } 
+    else if (userGuess < computerAnswer){
+        highLowMsg.textContent = "Your guess is too low.";
+
+        numGuesses.textContent = (parseInt(numGuesses.innerText) + 1)
+        if (parseInt(numGuesses.innerText) == 1) {
+            numGuessed.textContent = "Guessed numbers are: " + userGuess;
+        } else {
+            let sentence = numGuessed.innerText
+            numGuessed.textContent = sentence.slice(0,21) + sentence.slice(21) + ',' + userGuess;
+        }
+
+
+    } {
+
+        
     }
 
 
