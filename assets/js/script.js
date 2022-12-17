55cc1c348e23ad9ce9b5f3791ca71a189e94f5d3
PhilipOSull <philiposullivan11@gmail.com>
2022-12-17 13:17:51 +0000

    Guess button is restarting the timer everytime, include removeEventListener to fix this problem. Include the enter button to the game. Clear the User Guess value after every guess. Add text to the bottom area to keep the game area the same size before and after a guess.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,6 @@
-    // document.addEventListener("keydown", function (event) {
-    //     if (event.key === 'Enter') {
-    //         alert("test");
-    //     }
-    // });
+    
+
+    // MY GLOBAL VARIABLES //
 
     const btn = document.getElementById("btn--Guess");
     const btnRestart = document.getElementById("btn--Restart");
@@ -11,57 +9,84 @@
     const select = document.getElementById("difficulty");
     const hourGlass = document.getElementById("hour-glass");
     const highLowMsg = document.getElementById("high-low");
+    const numGuessed = document.getElementById("num-guessed");
+    const numGuesses = document.getElementById("num-guesses");
+    const userGuess = document.getElementById("guess-area");
 
     let computerAnswer = Math.floor(Math.random() * 100) + 1;
     let gameTimerId;
     let timer = 60;
 
+    
+
+    // MY EVENT LISTENERS //
+
     btn.addEventListener("click", evaluateGuess);
+    userGuess.addEventListener("keydown", evaluateGuessEnter);
     btnRestart.addEventListener("click", restart);
     btnStart.addEventListener("click", startGame);
+    
 
+    // FUNCTIONS //
+    function evaluateGuessEnter(e) {
+        if (e.key == 'Enter') {
+            evaluateGuess(e)
+        }
+    }
     function evaluateGuess(e) {
 
         e.preventDefault();
-        const numGuessed = document.getElementById("num-guessed");
-        const numGuesses = document.getElementById("num-guesses");
 
-        let userGuess = document.getElementById("guessArea").value;
-        if (userGuess < 1 || userGuess > 100) {
+        if (timeLeft.innerText == 60) {
+            highLowMsg.innerText = "Press the Play button!";
+            return;
+        }
+
+        const userGuessVal = userGuess.value;
+        console.log(userGuessVal)
+        userGuess.value='';
+        if (userGuessVal < 1 || userGuessVal > 100) {
             alert("Please enter a number between 1 and 100.");
-        } else if (userGuess < computerAnswer) {
+        } else if (userGuessVal < computerAnswer) {
             highLowMsg.textContent = "Your guess is too Low!";
 
             numGuesses.textContent = (parseInt(numGuesses.innerText) + 1);
             if (parseInt(numGuesses.innerText) == 1) {
-                numGuessed.textContent = "Guessed numbers are: " + userGuess;
+                numGuessed.textContent = "Guessed numbers are: " + userGuessVal;
             } else {
                 let sentence = numGuessed.innerText
-                numGuessed.textContent = sentence.slice(0, 21) + sentence.slice(21) + ',' + userGuess;
+                numGuessed.textContent = sentence.slice(0, 21) + sentence.slice(21) + ',' + userGuessVal;
             }
-        } else if (userGuess > computerAnswer) {
+        } else if (userGuessVal > computerAnswer) {
             highLowMsg.textContent = "Your guess is too High!";
 
             numGuesses.innerText = (parseInt(numGuesses.innerText) + 1);
             if (parseInt(numGuesses.innerText) == 1) {
-                numGuessed.textContent = "Guessed numbers are: " + userGuess;
+                numGuessed.textContent = "Guessed numbers are: " + userGuessVal;
             } else {
                 let sentence = numGuessed.innerText;
-                numGuessed.textContent = sentence.slice(0, 21) + sentence.slice(21) + ',' + userGuess;
+                numGuessed.textContent = sentence.slice(0, 21) + sentence.slice(21) + ',' + userGuessVal;
             }
-        } else if (userGuess == computerAnswer) {
+        } else if (userGuessVal == computerAnswer) {
 
             numGuesses.innerText = (parseInt(numGuesses.innerText) + 1);
             if (parseInt(numGuesses.innerText) == 1) {
                 highLowMsg.textContent = "Got it on your first go! You must be a psychic!";
-                numGuessed.textContent = "Guessed numbers are: " + userGuess;
+                numGuessed.textContent = "Guessed numbers are: " + userGuessVal;
+                clearInterval(gameTimerId);
+                btn.removeEventListener("click", evaluateGuess);
+                userGuess.removeEventListener("keydown", evaluateGuessEnter);
             } else {
                 highLowMsg.textContent = "Yay you got it right!";
                 let sentence = numGuessed.innerText;
-                numGuessed.textContent = sentence.slice(0, 21) + sentence.slice(21) + ',' + userGuess;
+                numGuessed.textContent = sentence.slice(0, 21) + sentence.slice(21) + ',' + userGuessVal;
+                clearInterval(gameTimerId);
+                btn.removeEventListener("click", evaluateGuess);
+                userGuess.removeEventListener("keydown", evaluateGuessEnter);
             }
         }
-        if (btnStart.style.visibility = "visible") {
+        if (btnStart.style.visibility == "visible") {
+            console.log("startGame", btnStart.style.visibility)
             startGame();
         }
     }
@@ -77,12 +102,14 @@
         };
         gameTimerId = setInterval(gameTimer, faster[selectValue]);
         btnStart.style.visibility = 'hidden';
+
+        btn.removeEventListener("click", evaluateGuess);
+        btn.addEventListener("click", evaluateGuess);
+        userGuess.removeEventListener("keydown", evaluateGuessEnter);
+        userGuess.addEventListener("keydown", evaluateGuessEnter);
     }
 
     function gameTimer() {
-        if (btnStart.style.visibility == 'visible') {
-            return;
-        }
 
         if (timer == 30) {
             hourGlass.innerHTML = `<i class="fas fa-hourglass-half"></i>`;
@@ -95,9 +122,8 @@
             clearInterval(gameTimerId)
             hourGlass.innerHTML = `<i class="fas fa-hourglass-end"></i>`;
             highLowMsg.textContent = "GAME OVER!";
-        }
-
-        ;
+            btnStart.style.visibility = "visible";
+        };
 
     }
 
@@ -105,11 +131,12 @@
         e.preventDefault()
         clearInterval(gameTimerId);
         hourGlass.innerHTML = `<i class="fas fa-hourglass-start"></i>`;
-        document.getElementById("num-guesses").innerText = 0;
-        document.getElementById("num-guessed").innerText = '';
-        document.getElementById("guess-area").value = '';
-        document.getElementById("high-low").innerText = '';
+        numGuesses.innerText = 0;
+        numGuessed.innerText = "Numbers Guessed: "
+        userGuess.value='';
+        highLowMsg.innerText = "Try your Luck!"
         computerAnswer = Math.floor(Math.random() * 100) + 1;
+        // console.log(computerAnswer);
         document.getElementById("time-left").innerText = 60;
         btnStart.style.visibility = 'visible';
 
